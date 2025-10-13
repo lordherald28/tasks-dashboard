945dbb1bda2e9894388c8f2263a8c53154538525
Gerardo Cárdenas Gonzále EXT <a3013@inycom.es>
2025-10-13 17:51:42 -0400

    feat: Crea Shell de layout y páginas TaskList/TaskForm con detección OnPush.

---
+++ src/app/features/tasks/pages/task-list/task-list.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './task-list.component.html',
+  styleUrl: './task-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TaskListComponent {
+
+}
