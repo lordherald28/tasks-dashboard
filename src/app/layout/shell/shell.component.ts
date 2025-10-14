14d5f04de78c1a6a24048bc5c490171b968076c2
Gerardo Cárdenas Gonzále EXT <a3013@inycom.es>
2025-10-14 02:09:10 -0400

    Refactor code structure for improved readability and maintainability

--- src/app/layout/shell/shell.component.ts
+++ src/app/layout/shell/shell.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'app-shell',
   standalone: true,
-  imports: [RouterOutlet, MatSlideToggleModule],
+  imports: [RouterOutlet, MatSlideToggleModule, MatIcon],
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss'],
 })
 export class ShellComponent {
-  toggleDark(on: boolean) { document.body.classList.toggle('dark-theme', on); }
+  constructor(private overlay: OverlayContainer) { }
+  // toggleDark(on: boolean) {
+  //   document.body.classList.toggle('dark-theme', on);
+  //   const overlayEl = this.overlay.getContainerElement();
+  //   overlayEl.classList.toggle('dark-theme', on);
+  // }
+
+  logout() {
+    // lógica logout: limpiar estado, redirigir a login, etc.
+    console.log('Logout clicked');
+  }
 }
